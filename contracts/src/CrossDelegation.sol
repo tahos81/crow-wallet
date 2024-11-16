79748bfde74e844c38eb8dd44538c42d9058c012
tahos81 <tahirozpala@gmail.com>
2024-11-17 00:11:21 +0700

    feat(contracts): primitive fill logic

--- contracts/src/CrossDelegation.sol
+++ contracts/src/CrossDelegation.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.26;
 
 import {IDestinationSettler} from "./interface/IDestinationSettler.sol";
-import {CrowOrderData} from "./ERC7683Crow.sol";
+import {CrowOrderData, XCall} from "./ERC7683Crow.sol";
+import {Secp256r1} from "./libraries/Secp256r1.sol";
 
 struct CrowOrderWithSig {
     CrowOrderData orderData;
@@ -10,5 +11,40 @@ struct CrowOrderWithSig {
 }
 
 contract CrossDelegation is IDestinationSettler {
-    function fill(bytes32 orderId, bytes calldata originData, bytes calldata fillerData) external override {}
+    /// @notice The x coordinate of the authorized public key
+    uint256 authorizedPublicKeyX;
+    /// @notice The y coordinate of the authorized public key
+    uint256 authorizedPublicKeyY;
+
+    /// @notice Authorizes provided public key to transact on behalf of this account. Only callable by EOA itself.
+    function authorize(uint256 publicKeyX, uint256 publicKeyY) public {
+        require(msg.sender == address(this));
+
+        authorizedPublicKeyX = publicKeyX;
+        authorizedPublicKeyY = publicKeyY;
+    }
+
+    function fill(bytes32 orderId, bytes calldata originData, bytes calldata /* fillerData */ ) external override {
+        require(keccak256(abi.encode(originData, block.chainid)) == orderId, "invalid order id");
+
+        // Ensure that the call is not malformed. If the call is malformed, abi.decode will fail.
+        CrowOrderWithSig memory orderWithSig = abi.decode(originData, (CrowOrderWithSig));
+
+        (bytes32 r, bytes32 s) = abi.decode(orderWithSig.signature, (bytes32, bytes32));
+
+        require(Secp256r1.verify(orderId, r, s, authorizedPublicKeyX, authorizedPublicKeyY), "Invalid signature");
+
+        _execute(orderWithSig.orderData);
+    }
+
+    function _execute(CrowOrderData memory orderData) internal {
+        for (uint256 i = 0; i < orderData.xCalls.length; i++) {
+            XCall memory xCall = orderData.xCalls[i];
+            (bool success,) = xCall.target.call{value: xCall.value}(xCall.callData);
+            require(success, "xCall failed");
+        }
+    }
+
+    fallback() external payable {}
+    receive() external payable {}
 }
